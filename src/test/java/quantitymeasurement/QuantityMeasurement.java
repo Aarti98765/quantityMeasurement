0bace86c1531621b22f09058867c533a1738a7dd
Aarti98765 <aartiahireengit@gmail.com>
2020-04-21 14:05:32 +0530

    ADDED: Code to given different values to different object of same class should return not equal

--- src/test/java/quantitymeasurement/QuantityMeasurement.java
+++ src/test/java/quantitymeasurement/QuantityMeasurement.java
@@ -10,4 +10,11 @@ public class QuantityMeasurement {
         Feet feet2 = new Feet(0.0);
         Assert.assertEquals(feet1,feet2);
     }
+
+    @Test
+    public void given0FeetAnd1Feet_shouldReturnEqual() {
+        Feet feet1 = new Feet(0.0);
+        Feet feet2 = new Feet(1.0);
+        Assert.assertNotEquals(feet1,feet2);
+    }
 }
