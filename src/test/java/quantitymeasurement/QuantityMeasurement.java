eac377290cab775669b9b6367faf54a059a00189
Aarti98765 <aartiahireengit@gmail.com>
2020-04-21 14:14:55 +0530

    ADDED: Code to given same values to different object of class inch should return equal

--- src/test/java/quantitymeasurement/QuantityMeasurement.java
+++ src/test/java/quantitymeasurement/QuantityMeasurement.java
@@ -12,9 +12,22 @@ public class QuantityMeasurement {
     }
 
     @Test
-    public void given0FeetAnd1Feet_shouldReturnEqual() {
+    public void given0FeetAnd1Feet_shouldReturnNotEqual() {
         Feet feet1 = new Feet(0.0);
         Feet feet2 = new Feet(1.0);
         Assert.assertNotEquals(feet1,feet2);
     }
+
+    @Test
+    public void given1InchAnd1Inch_shouldReturnEqual() {
+        Inch inch1 = new Inch(1.0);
+        Inch inch2 = new Inch(1.0);
+    }
+
+   // @Test
+    //public void given0InchAnd1Inch_shouldReturnNotEqual() {
+      //  Inch inch1 = new Inch(0.0);
+        //Inch inch2 = new Inch(1.0);
+        //Assert.assertNotEquals(inch1,inch2);
+   // }
 }
