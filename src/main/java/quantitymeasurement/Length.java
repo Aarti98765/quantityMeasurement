4699a733e771bb5340a2578df13c17b5a09bf01d
Aarti98765 <aartiahireengit@gmail.com>
2020-04-21 14:42:11 +0530

    REFACTORED: Code as dry is voilated for feet and inch with length and unit differentiated

---
+++ src/main/java/quantitymeasurement/Length.java
@@ -0,0 +1,24 @@
+package quantitymeasurement;
+
+import java.util.Objects;
+
+ public class Length {
+    private final double value;
+    private final Unit unit;
+
+    enum Unit{FEET, INCH};
+    public Length(Unit unit, double value) {
+        this.unit = unit;
+        this.value = value;
+    }
+
+     @Override
+     public boolean equals(Object o) {
+         if (this == o) return true;
+         if (o == null || getClass() != o.getClass()) return false;
+         Length length = (Length) o;
+         return Double.compare(length.value, value) == 0 &&
+                 unit == length.unit;
+     }
+
+ }
